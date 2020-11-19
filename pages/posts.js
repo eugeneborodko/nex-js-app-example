a00092046f01804bc0745263c68a24ce9e21eecc
evgenborod496 <eugeneborodko@gmail.com>
2020-11-19 12:32:14 +0300

    feat: add posts links

--- pages/posts.js
+++ pages/posts.js
@@ -1,11 +1,34 @@
+import Link from 'next/link'
+
 import Layout from "../components/layout"
 
-const Posts = () => {
+const Posts = ({ posts }) => {
   return (
     <Layout title={'Posts Page'}>
       <h1 className="title title_center">Posts Page</h1>
+      <ul className="posts-list">
+        {
+          posts.map(post => {
+            return (
+              <li className="posts-list__item" key={post.id}>
+                <Link href={'/post/[id]'} as={`/post/${post.id}`}>
+                  <a className="posts-list__link">{post.title}</a>
+                </Link>
+              </li>
+            )
+          })
+        }
+      </ul>
     </Layout>
   )
+}
+
+Posts.getInitialProps = async () => {
+  const response = await fetch('http://localhost:3000/api/get-content/posts')
+  const posts = await response.json()
+  return {
+    posts
+  }
 }
 
 export default Posts
