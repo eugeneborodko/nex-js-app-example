71beb4cbe501391e76eb984dafe5272b93ed37d4
evgenborod496 <eugeneborodko@gmail.com>
2020-11-19 12:21:15 +0300

    feat: add data to server

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout title={'About Page'}>
+      <h1 className="title title_center">About Page</h1>
+    </Layout>
+  )
+}
+
+export default About
---
+++ pages/posts.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout"
+
+const Posts = () => {
+  return (
+    <Layout title={'Posts Page'}>
+      <h1 className="title title_center">Posts Page</h1>
+    </Layout>
+  )
+}
+
+export default Posts
